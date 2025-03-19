988676a6cc2b7a3739d2774435fa81d4573a1f94
Vasco Figueira <vasco.figueira@gmail.com>
2025-03-19 16:54:56 +0000

    surface fibonacci, rename program, prune cruft

--- script/build.rs
+++ script/build.rs
@@ -1,5 +1,6 @@
 use sp1_build::build_program_with_args;
 
 fn main() {
-    build_program_with_args("../program", Default::default())
+    build_program_with_args("../aggregator", Default::default());
+    build_program_with_args("../fibonacci", Default::default());
 }
--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -1,92 +1,113 @@
-//! An end-to-end example of using the SP1 SDK to generate a proof of a program that can be executed
-//! or have a core proof generated.
-//!
-//! You can run this script using the following command:
-//! ```shell
-//! RUST_LOG=info cargo run --release -- --execute
-//! ```
-//! or
-//! ```shell
-//! RUST_LOG=info cargo run --release -- --prove
-//! ```
+//! A simple example showing how to aggregate proofs of multiple programs with SP1.
 
-use alloy_sol_types::SolType;
-use clap::Parser;
-use fibonacci_lib::PublicValuesStruct;
-use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
+use sp1_sdk::{
+    include_elf, HashableKey, ProverClient, SP1Proof, SP1ProofWithPublicValues, SP1Stdin,
+    SP1VerifyingKey,
+};
 
-/// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci-program");
+/// A program that aggregates the proofs of the simple program.
+const AGGREGATION_ELF: &[u8] = include_elf!("aggregator");
 
-/// The arguments for the command.
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(long)]
-    execute: bool,
+// A program that just runs a simple computation.
+const FIBONACCI_ELF: &[u8] = include_elf!("fibonacci");
 
-    #[clap(long)]
-    prove: bool,
-
-    #[clap(long, default_value = "20")]
-    n: u32,
+/// An input to the aggregation program.
+///
+/// Consists of a proof and a verification key.
+struct AggregationInput {
+    pub proof: SP1ProofWithPublicValues,
+    pub vk: SP1VerifyingKey,
 }
 
 fn main() {
     // Setup the logger.
     sp1_sdk::utils::setup_logger();
-    dotenv::dotenv().ok();
 
-    // Parse the command line arguments.
-    let args = Args::parse();
-
-    if args.execute == args.prove {
-        eprintln!("Error: You must specify either --execute or --prove");
-        std::process::exit(1);
-    }
-
-    // Setup the prover client.
+    // Initialize the proving client.
     let client = ProverClient::from_env();
 
-    // Setup the inputs.
-    let mut stdin = SP1Stdin::new();
-    stdin.write(&args.n);
+    // Setup the proving and verifying keys.
+    let (aggregation_pk, _) = client.setup(AGGREGATION_ELF);
+    let (fibonacci_pk, fibonacci_vk) = client.setup(FIBONACCI_ELF);
 
-    println!("n: {}", args.n);
-
-    if args.execute {
-        // Execute the program
-        let (output, report) = client.execute(FIBONACCI_ELF, &stdin).run().unwrap();
-        println!("Program executed successfully.");
-
-        // Read the output.
-        let decoded = PublicValuesStruct::abi_decode(output.as_slice(), true).unwrap();
-        let PublicValuesStruct { n, a, b } = decoded;
-        println!("n: {}", n);
-        println!("a: {}", a);
-        println!("b: {}", b);
-
-        let (expected_a, expected_b) = fibonacci_lib::fibonacci(n);
-        assert_eq!(a, expected_a);
-        assert_eq!(b, expected_b);
-        println!("Values are correct!");
-
-        // Record the number of cycles executed.
-        println!("Number of cycles: {}", report.total_instruction_count());
-    } else {
-        // Setup the program for proving.
-        let (pk, vk) = client.setup(FIBONACCI_ELF);
-
-        // Generate the proof
-        let proof = client
-            .prove(&pk, &stdin)
+    // Generate the fibonacci proofs.
+    let proof_1 = tracing::info_span!("generate fibonacci proof n=10").in_scope(|| {
+        let mut stdin = SP1Stdin::new();
+        stdin.write(&10);
+        client
+            .prove(&fibonacci_pk, &stdin)
+            .compressed()
             .run()
-            .expect("failed to generate proof");
+            .expect("proving failed")
+    });
+    let proof_2 = tracing::info_span!("generate fibonacci proof n=20").in_scope(|| {
+        let mut stdin = SP1Stdin::new();
+        stdin.write(&20);
+        client
+            .prove(&fibonacci_pk, &stdin)
+            .compressed()
+            .run()
+            .expect("proving failed")
+    });
+    let proof_3 = tracing::info_span!("generate fibonacci proof n=30").in_scope(|| {
+        let mut stdin = SP1Stdin::new();
+        stdin.write(&30);
+        client
+            .prove(&fibonacci_pk, &stdin)
+            .compressed()
+            .run()
+            .expect("proving failed")
+    });
 
-        println!("Successfully generated proof!");
+    // Setup the inputs to the aggregation program.
+    let input_1 = AggregationInput {
+        proof: proof_1,
+        vk: fibonacci_vk.clone(),
+    };
+    let input_2 = AggregationInput {
+        proof: proof_2,
+        vk: fibonacci_vk.clone(),
+    };
+    let input_3 = AggregationInput {
+        proof: proof_3,
+        vk: fibonacci_vk.clone(),
+    };
+    let inputs = vec![input_1, input_2, input_3];
 
-        // Verify the proof.
-        client.verify(&proof, &vk).expect("failed to verify proof");
-        println!("Successfully verified proof!");
-    }
+    // Aggregate the proofs.
+    tracing::info_span!("aggregate the proofs").in_scope(|| {
+        let mut stdin = SP1Stdin::new();
+
+        // Write the verification keys.
+        let vkeys = inputs
+            .iter()
+            .map(|input| input.vk.hash_u32())
+            .collect::<Vec<_>>();
+        stdin.write::<Vec<[u32; 8]>>(&vkeys);
+
+        // Write the public values.
+        let public_values = inputs
+            .iter()
+            .map(|input| input.proof.public_values.to_vec())
+            .collect::<Vec<_>>();
+        stdin.write::<Vec<Vec<u8>>>(&public_values);
+
+        // Write the proofs.
+        //
+        // Note: this data will not actually be read by the aggregation program, instead it will be
+        // witnessed by the prover during the recursive aggregation process inside SP1 itself.
+        for input in inputs {
+            let SP1Proof::Compressed(proof) = input.proof.proof else {
+                panic!()
+            };
+            stdin.write_proof(*proof, input.vk.vk);
+        }
+
+        // Generate the plonk bn254 proof.
+        client
+            .prove(&aggregation_pk, &stdin)
+            .plonk()
+            .run()
+            .expect("proving failed");
+    });
 }
